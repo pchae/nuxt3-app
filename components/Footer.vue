f12ed61cab1b7ea18073dc16f8012b15375434f9
pchae <20016424+pchae@users.noreply.github.com>
2022-09-13 02:27:47 -0400

    update layout and links

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,14 +1,25 @@
 <template>
   <footer class="footer">
-    <div class="content has-text-centered">
-      <p>
-        <strong>&#169; 2016-2022 Saturdays.io, Inc.</strong> The source code is licensed
-        <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.
-      </p>
+    <div class="level is-mobile">
+        <div class=" has-text-left-mobile">
+            <p>
+                <strong>&#169; 2016-2022 Phil Choi.</strong>
+            </p>
+        </div>
+        <div class="level has-text-right-mobile">
+            <p class="level-item has-text-centered">
+                <a href="https://linkedin.com/in/pchoi1/">LinkedIn</a>
+            </p>
+            <p class="level-item has-text-centered">
+                <a href="mailto:contact@philchoi.ca">Email</a>
+            </p>
+        </div>
     </div>
   </footer>
 </template>
 
-<script setup></script>
+<script setup>
+</script>
 
-<style></style>
+<style>
+</style>

5cb9df7a25ff2f8f4202a86e05ee91356491237c
pchae <20016424+pchae@users.noreply.github.com>
2022-09-15 19:45:09 -0400

    update colour style

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,10 +1,10 @@
 <template>
-  <section class="hero is-info">
+  <section class="hero has-background-black-ter">
     <div class="hero-body">
-      <h1 class="title">
+      <h1 class="title has-text-white">
         Phil Choi is a Product Designer based in Toronto.
       </h1>
-      <h2 class="subtitle">
+      <h2 class="subtitle has-text-grey-light">
         Principal Product Designer at <a href="https://flipboard.com/">Flipboard</a>. Previously at <a href="https://momentumdesignlab.com">Momentum</a> & RBC Ventures.
       </h2>
     </div>

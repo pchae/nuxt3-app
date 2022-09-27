84954f2dbbd5cf92c75fe8986e40eec120f67332
pchae <20016424+pchae@users.noreply.github.com>
2022-09-27 18:03:27 -0400

    Add scrollTo top script

--- app.vue
+++ app.vue
@@ -1,3 +1,14 @@
 <template>
   <NuxtPage />
 </template>
+
+<script setup>
+  const nuxtApp = useNuxtApp()
+
+  nuxtApp.hook("page:finish", () => {
+    window.scrollTo(0, 0)
+  })
+</script>
+
+<style>
+</style>

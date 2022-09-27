7a8d2bf6dc417ff7922da8303855307464171077
pchae <20016424+pchae@users.noreply.github.com>
2022-09-27 17:14:15 -0400

    update nav-links to NuxtLink tags

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,30 +1,44 @@
 <template>
-  <footer class="footer has-background-black">
-    <div class="level is-mobile">
-        <div class=" has-text-left-mobile">
-            <p class="">
-                <strong class="has-text-white">&#169; 2014-2022 Phil Choi.</strong>
-            </p>
+    <footer class="footer has-background-black">
+        <div class="level is-mobile">
+            <div class="has-text-left-mobile">
+                <p class="">
+                    <strong class="has-text-white">&#169; 2014-2022 Phil Choi.</strong>
+                </p>
+            </div>
+            <div class="level has-text-right-mobile is-mobile">
+                <div class="level-item">
+                    <NuxtLink to="https://linkedin.com/in/pchoi1/" target="_blank" rel="noopener" class="navbar-item">
+                        LinkedIn
+                    </NuxtLink>
+
+                </div>
+                <div class="level-item">
+
+                    <NuxtLink to="mailto:contact@philchoi.ca" target="_blank" rel="noopener" class="navbar-item">
+                        Email
+                    </NuxtLink>
+                </div>
+            </div>
         </div>
-        <div class="level has-text-right-mobile is-mobile">
-            <p class="level-item pr-2">
-                <a href="https://linkedin.com/in/pchoi1/">LinkedIn</a>
-            </p>
-            <p class="level-item">
-                <a href="mailto:contact@philchoi.ca">Email</a>
-            </p>
-        </div>
-    </div>
-  </footer>
+    </footer>
 </template>
 
 <script>
 </script>
 
 <style lang="scss" scoped>
+@import '~/assets/styles/_variables.scss';
 
-    .footer {
-        padding: 3rem 1.5rem;
+.footer {
+    padding: 3rem 1rem;
+}
+
+.navbar-item {
+    color: $blue;
+
+    &:hover {
+        background: none;
     }
-
+}
 </style>

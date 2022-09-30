c8c864b8fad6f28b8e44928bf2e544dfbcdc383c
pchae <20016424+pchae@users.noreply.github.com>
2022-09-30 11:01:57 -0400

    update page ti tle

--- pages/404.vue
+++ pages/404.vue
@@ -2,7 +2,7 @@
     <div>
 
         <Head>
-            <Title>Phil Choi - 404 Page not found</Title>
+            <Title>Phil Choi - Page not found</Title>
             <Meta name="description" content="Phil Choi - 404 error not found" />
         </Head>
         <Nav />

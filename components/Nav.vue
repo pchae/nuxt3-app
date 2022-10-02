7b7b8833026f39edfa1b74e4539302f7870c2e48
pchae <20016424+pchae@users.noreply.github.com>
2022-10-02 15:33:50 -0400

    update router links

--- components/Nav.vue
+++ components/Nav.vue
@@ -59,7 +59,13 @@
 
         <div class="navbar-item">
           <NuxtLink to="/tos" active-class="active">
-            <h2 class="is-size-3-tablet is-size-4-mobile">Experiment</h2>
+            <h2 class="is-size-3-tablet is-size-4-mobile">Terms of Service</h2>
+          </NuxtLink>
+        </div>
+
+        <div class="navbar-item">
+          <NuxtLink to="/privacy" active-class="active">
+            <h2 class="is-size-3-tablet is-size-4-mobile">Privacy Policy</h2>
           </NuxtLink>
         </div>
       </div>

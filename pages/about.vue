59b0bf332c2ba3934df7f75d3b83c6023f2d1243
pchae <20016424+pchae@users.noreply.github.com>
2022-10-04 02:40:18 -0400

    Move client and contact sections into SFCs

--- pages/about.vue
+++ pages/about.vue
@@ -1,191 +1,48 @@
 <template>
-    <div class="">
-        <Head>
-            <Title>Phil Choi - About</Title>
-            <Meta name="description" content="About Phil Choi, Product Designer based in Toronto"/>
-        </Head>
-        <Nav />
 
-        <section id="bio" class="container is-widescreen py-6 px-4">
-            <div class="content is-medium">
-                <h1 class="title has-text-white">My focus is on art, design, and technology as well as when, where, and how they affect everyday life.</h1>
-            </div>
-            <div class="content">
-                <p class="is-size-5 has-text-weight-normal has-text-grey-light">As an 80s baby, I hold a unique perspective due to maturing in parallel with the shift from analog to digital technologies. The result is that I'm able to bridge the cultural, ideological, and technological gaps between older generations and today's youth.</p>
-            </div>
-            <div class="content">
-                <p class="is-size-5 has-text-weight-normal has-text-grey-light">Over the past decade I've designed and built solutions for North American digital agencies, brands, startups, FinTech, and Software as a Service platforms. My Design process incorporates <a href="https://www.ted.com/talks/david_kelley_human_centered_design" rel="noreferrer" target="_blank">Human-centered design principles</a>, the <a href="https://agilemanifesto.org/" rel="noreferrer" target="_blank">Agile Manifesto</a>, Diversity & Inclusion for all, scalable Design systems, <a href="https://www.goodreads.com/en/book/show/29939161-radical-candor" rel="noreferrer" target="_blank">Radical Candor</a>, and <a href="https://www.forbes.com/sites/victorlipman/2014/10/15/phil-jackson-brings-ny-knicks-mindful-management/" rel="noreferrer" target="_blank">Mindful Management</a> practices. As a person with an 'Invisible disability' of partial vision loss, I strongly advocate UX for the differently-abled and <a href="https://www.w3.org/WAI/business-case/" rel="noreferrer" target="_blank">Accessibility for All</a>.</p>
-            </div>
-            <div class="content">
-                <p class="is-size-5 has-text-weight-normal has-text-grey-light">Platforms I've designed for include: Android, iOS, Roku, Amazon Fire TV, Smart TVs, Playstation 4, Windows Devices, and enterprise SaaS.</p>
-            </div>
-        </section>
+    <Head>
+        <Title>Phil Choi - About</Title>
+        <Meta name="description" content="About Phil Choi, Product Designer based in Toronto" />
+    </Head>
+    <Nav />
 
-        <section id="clients" class="container py-6 px-4">
-            <div class="content has-text-centered mt-6">
-                <h1 class="title has-text-white">Past Clients</h1>
-            </div>
+    <section id="bio" class="container is-widescreen py-6 px-4">
+        <div class="content is-medium">
+            <h1 class="title has-text-white">My focus is on art, design, and technology as well as when, where, and how
+                they affect everyday life.</h1>
+        </div>
+        <div class="content">
+            <p class="is-size-5 has-text-weight-normal has-text-grey-light">As an 80s baby, I hold a unique perspective
+                due to maturing in parallel with the shift from analog to digital technologies. The result is that I'm
+                able to bridge the cultural, ideological, and technological gaps between older generations and today's
+                youth.</p>
+        </div>
+        <div class="content">
+            <p class="is-size-5 has-text-weight-normal has-text-grey-light">Over the past decade I've designed and built
+                solutions for North American digital agencies, brands, startups, FinTech, and Software as a Service
+                platforms. My Design process incorporates <a
+                    href="https://www.ted.com/talks/david_kelley_human_centered_design" rel="noreferrer"
+                    target="_blank">Human-centered design principles</a>, the <a href="https://agilemanifesto.org/"
+                    rel="noreferrer" target="_blank">Agile Manifesto</a>, Diversity & Inclusion for all, scalable Design
+                systems, <a href="https://www.goodreads.com/en/book/show/29939161-radical-candor" rel="noreferrer"
+                    target="_blank">Radical Candor</a>, and <a
+                    href="https://www.forbes.com/sites/victorlipman/2014/10/15/phil-jackson-brings-ny-knicks-mindful-management/"
+                    rel="noreferrer" target="_blank">Mindful Management</a> practices. As a person with an 'Invisible
+                disability' of partial vision loss, I strongly advocate UX for the differently-abled and <a
+                    href="https://www.w3.org/WAI/business-case/" rel="noreferrer" target="_blank">Accessibility for
+                    All</a>.</p>
+        </div>
+        <div class="content">
+            <p class="is-size-5 has-text-weight-normal has-text-grey-light">Platforms I've designed for include:
+                Android, iOS, Roku, Amazon Fire TV, Smart TVs, Playstation 4, Windows Devices, and enterprise SaaS.</p>
+        </div>
+    </section>
 
-            <div id="row1" class="columns">
-                <div class="column">
-                    <div class="columns is-mobile is-variable is-1-mobile">
-                        <div class="column p-6">
-                            <figure class="image container">
-                                <img src="/assets/logo/cbc.svg" alt="Canadian Broadcasting Corporation logo">
-                            </figure>
-                        </div>
+    <Client />
 
-                        <div class="column p-6">
-                            <figure class="image container">
-                                <img src="/assets/logo/usatoday.svg" alt="USA Today logo">
-                            </figure>
-                        </div>
-                    </div>
-                </div>
+    <Contact />
 
-                <div class="column">
-                    <div class="columns is-mobile is-variable is-1-mobile">
-                        <div class="column p-6">
-                            <figure class="image container">
-                                <img src="/assets/logo/reuters.svg" alt="Reuters logo">
-                            </figure>
-                        </div>
-
-                        <div class="column p-6">
-                            <figure class="image container">
-                                <img src="/assets/logo/bloomberg.svg" alt="Bloomberg logo">
-                            </figure>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-
-            <div id="row2" class="columns">
-                <div class="column">
-                    <div class="columns is-mobile is-variable is-1-mobile">
-                        <div class="column p-6">
-                            <figure class="image container">
-                                <img src="/assets/logo/starz.svg" alt="STARZ logo">
-                            </figure>
-                        </div>
-
-                        <div class="column p-6">
-                            <figure class="image container">
-                                <img src="/assets/logo/pbs.svg" alt="Public Broadcasting Service logo">
-                            </figure>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="column">
-                    <div class="columns is-mobile is-variable is-1-mobile">
-                        <div class="column p-6">
-                            <figure class="image container">
-                                <img src="/assets/logo/spc.svg" alt="Student Price Card logo">
-                            </figure>
-                        </div>
-
-                        <div class="column p-6">
-                            <figure class="image container">
-                                <img src="/assets/logo/redbull.svg" alt="Redbull logo">
-                            </figure>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-
-            <div id="row3" class="columns">
-                <div class="column">
-                    <div class="columns is-mobile is-variable is-1-mobile">
-                        <div class="column p-6">
-                            <figure class="image container">
-                                <img src="/assets/logo/ogilvy.svg" alt="Ogilvy logo">
-                            </figure>
-                        </div>
-
-                        <div class="column p-6">
-                            <figure class="image container">
-                                <img src="/assets/logo/plastic.svg" alt="Plastic Mobile logo">
-                            </figure>
-                        </div>                        
-                    </div>
-                </div>
-
-                <div class="column">
-                    <div class="columns is-mobile is-variable is-1-mobile">
-                        <div class="column p-6">
-                            <figure class="image container">
-                                <img src="/assets/logo/bnotions.svg" alt="Symbility logo">
-                            </figure>
-                        </div>
-
-                        <div class="column p-6">
-                            <figure class="image container">
-                                <img src="/assets/logo/twitch.svg" alt="Twitch.tv logo">
-                            </figure>
-                        </div>                     
-                    </div>
-                </div>
-            </div>
-
-
-            <div id="row4" class="columns">
-                <div class="column">
-                    <div class="columns is-mobile is-variable is-1-mobile">
-                        <div class="column p-6">
-                            <figure class="image container">
-                                <img src="/assets/logo/dell.svg" alt="Dell logo">
-                            </figure>
-                        </div>
-
-                        <div class="column p-6">
-                            <figure class="image container">
-                                <img src="/assets/logo/microsoft.svg" alt="Microsoft logo">
-                            </figure>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="column">
-                    <div class="columns is-mobile is-variable is-1-mobile">
-                        <div class="column p-6">
-                            <figure class="image container">
-                                <img src="/assets/logo/cisco.svg" alt="Cisco logo">
-                            </figure>
-                        </div>
-
-                        <div class="column p-6">
-                            <figure class="image container">
-                                <img src="/assets/logo/latam.svg" alt="LATAM Airlines logo">
-                            </figure>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-        </section>
-
-        <section id="cta" class=" mb-0">
-            <div class="container py-6">
-                <div class="has-text-centered pb-4">
-                    <h1 class="title has-text-white">Say Hello</h1>
-                </div>
-                <div class="level is-centered">
-                    <div class="level-item mb-6">
-                        <NuxtLink to="mailto:contact@philchoi.ca">
-                            <button class="button is-link is-medium is-outlined">
-                                <p><strong>Send an email</strong></p>
-                            </button>
-                        </NuxtLink>
-                    </div>
-                </div>
-            </div>
-        </section>
-
-        <Footer />        
-    </div>
+    <Footer />
 </template>
 
 
@@ -194,17 +51,17 @@
 
 
 <style lang="scss" scoped>
-    @import '~/assets/styles/_variables.scss';
+@import '~/assets/styles/_variables.scss';
 
-    .button.is-link.is-outlined {
-        color: $blue;
+.button.is-link.is-outlined {
+    color: $blue;
+    background: none;
+    border-color: $blue;
+
+    &:hover {
+        color: $red;
         background: none;
-        border-color: $blue;
-        &:hover {
-            color: $red;
-            background: none;
-            border-color: $red;
-        }
+        border-color: $red;
     }
-
+}
 </style>

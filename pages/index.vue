74cd8b4e3dcfe81dd3eabd57130eee0f07e7bd6d
pchae <20016424+pchae@users.noreply.github.com>
2022-10-04 15:10:03 -0400

    Added temp section to project template

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,27 @@
 <template>
     <div>
+
         <Head>
             <Title>Phil Choi - Product Designer</Title>
-            <Meta name="description" content="Phil Choi - Product Design portfolio"/>
+            <Meta name="description" content="Phil Choi - Product Design portfolio" />
         </Head>
 
         <Nav />
 
         <Hero />
+
+        <section class="hero has-background-primary">
+            <div class="hero-body">
+                <h1 class="title has-text-white">
+                    Engage Content Studio
+                </h1>
+                <NuxtLink to="/project">
+                    <button class="button is-link is-medium">
+                        <p><strong>View project</strong></p>
+                    </button>
+                </NuxtLink>
+            </div>
+        </section>
 
         <Footer />
 
@@ -20,4 +34,5 @@
 
 
 <style>
+
 </style>

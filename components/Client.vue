4da4e652339c00f681978cd158c18cec70548c76
pchae <20016424+pchae@users.noreply.github.com>
2022-10-04 02:51:44 -0400

    remove placeholder parameter

--- components/Client.vue
+++ components/Client.vue
@@ -9,13 +9,13 @@
                 <div class="columns is-mobile is-variable is-1-mobile">
                     <div class="column p-6">
                         <figure class="image container">
-                            <nuxt-img src="/assets/logo/cbc.svg" alt="Canadian Broadcasting Corporation logo" placeholder />
+                            <nuxt-img src="/assets/logo/cbc.svg" alt="Canadian Broadcasting Corporation logo" />
                         </figure>
                     </div>
 
                     <div class="column p-6">
                         <figure class="image container">
-                            <nuxt-img src="/assets/logo/usatoday.svg" alt="USA Today logo" placeholder />
+                            <nuxt-img src="/assets/logo/usatoday.svg" alt="USA Today logo" />
                         </figure>
                     </div>
                 </div>
@@ -25,13 +25,13 @@
                 <div class="columns is-mobile is-variable is-1-mobile">
                     <div class="column p-6">
                         <figure class="image container">
-                            <nuxt-img src="/assets/logo/reuters.svg" alt="Reuters logo" placeholder />
+                            <nuxt-img src="/assets/logo/reuters.svg" alt="Reuters logo" />
                         </figure>
                     </div>
 
                     <div class="column p-6">
                         <figure class="image container">
-                            <nuxt-img src="/assets/logo/bloomberg.svg" alt="Bloomberg logo" placeholder />
+                            <nuxt-img src="/assets/logo/bloomberg.svg" alt="Bloomberg logo" />
                         </figure>
                     </div>
                 </div>
@@ -44,13 +44,13 @@
                 <div class="columns is-mobile is-variable is-1-mobile">
                     <div class="column p-6">
                         <figure class="image container">
-                            <nuxt-img src="/assets/logo/starz.svg" alt="STARZ logo" placeholder />
+                            <nuxt-img src="/assets/logo/starz.svg" alt="STARZ logo" />
                         </figure>
                     </div>
 
                     <div class="column p-6">
                         <figure class="image container">
-                            <nuxt-img src="/assets/logo/pbs.svg" alt="Public Broadcasting Service logo" placeholder />
+                            <nuxt-img src="/assets/logo/pbs.svg" alt="Public Broadcasting Service logo" />
                         </figure>
                     </div>
                 </div>
@@ -60,13 +60,13 @@
                 <div class="columns is-mobile is-variable is-1-mobile">
                     <div class="column p-6">
                         <figure class="image container">
-                            <nuxt-img src="/assets/logo/spc.svg" alt="Student Price Card logo" placeholder />
+                            <nuxt-img src="/assets/logo/spc.svg" alt="Student Price Card logo" />
                         </figure>
                     </div>
 
                     <div class="column p-6">
                         <figure class="image container">
-                            <nuxt-img src="/assets/logo/redbull.svg" alt="Redbull logo" placeholder />
+                            <nuxt-img src="/assets/logo/redbull.svg" alt="Redbull logo" />
                         </figure>
                     </div>
                 </div>
@@ -79,13 +79,13 @@
                 <div class="columns is-mobile is-variable is-1-mobile">
                     <div class="column p-6">
                         <figure class="image container">
-                            <nuxt-img src="/assets/logo/ogilvy.svg" alt="Ogilvy logo" placeholder />
+                            <nuxt-img src="/assets/logo/ogilvy.svg" alt="Ogilvy logo" />
                         </figure>
                     </div>
 
                     <div class="column p-6">
                         <figure class="image container">
-                            <nuxt-img src="/assets/logo/plastic.svg" alt="Plastic Mobile logo" placeholder />
+                            <nuxt-img src="/assets/logo/plastic.svg" alt="Plastic Mobile logo" />
                         </figure>
                     </div>
                 </div>
@@ -95,13 +95,13 @@
                 <div class="columns is-mobile is-variable is-1-mobile">
                     <div class="column p-6">
                         <figure class="image container">
-                            <nuxt-img src="/assets/logo/bnotions.svg" alt="Symbility logo" placeholder />
+                            <nuxt-img src="/assets/logo/bnotions.svg" alt="Symbility logo" />
                         </figure>
                     </div>
 
                     <div class="column p-6">
                         <figure class="image container">
-                            <nuxt-img src="/assets/logo/twitch.svg" alt="Twitch.tv logo" placeholder />
+                            <nuxt-img src="/assets/logo/twitch.svg" alt="Twitch.tv logo" />
                         </figure>
                     </div>
                 </div>
@@ -114,13 +114,13 @@
                 <div class="columns is-mobile is-variable is-1-mobile">
                     <div class="column p-6">
                         <figure class="image container">
-                            <nuxt-img src="/assets/logo/dell.svg" alt="Dell logo" placeholder />
+                            <nuxt-img src="/assets/logo/dell.svg" alt="Dell logo" />
                         </figure>
                     </div>
 
                     <div class="column p-6">
                         <figure class="image container">
-                            <nuxt-img src="/assets/logo/microsoft.svg" alt="Microsoft logo" placeholder />
+                            <nuxt-img src="/assets/logo/microsoft.svg" alt="Microsoft logo" />
                         </figure>
                     </div>
                 </div>
@@ -130,13 +130,13 @@
                 <div class="columns is-mobile is-variable is-1-mobile">
                     <div class="column p-6">
                         <figure class="image container">
-                            <nuxt-img src="/assets/logo/cisco.svg" alt="Cisco logo" placeholder />
+                            <nuxt-img src="/assets/logo/cisco.svg" alt="Cisco logo" />
                         </figure>
                     </div>
 
                     <div class="column p-6">
                         <figure class="image container">
-                            <nuxt-img src="/assets/logo/latam.svg" alt="LATAM Airlines logo" placeholder />
+                            <nuxt-img src="/assets/logo/latam.svg" alt="LATAM Airlines logo" />
                         </figure>
                     </div>
                 </div>

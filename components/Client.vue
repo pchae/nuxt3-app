56e10a7d156e5e9a6f59003b84888a44d7beb060
pchae <20016424+pchae@users.noreply.github.com>
2022-10-04 02:39:56 -0400

    Create file

---
+++ components/Client.vue
@@ -0,0 +1,152 @@
+<template>
+    <section id="clients" class="container py-6 px-4">
+        <div class="content has-text-centered mt-6">
+            <h1 class="title has-text-white">Past Clients</h1>
+        </div>
+
+        <div id="row1" class="columns">
+            <div class="column">
+                <div class="columns is-mobile is-variable is-1-mobile">
+                    <div class="column p-6">
+                        <figure class="image container">
+                            <img src="/assets/logo/cbc.svg" alt="Canadian Broadcasting Corporation logo">
+                        </figure>
+                    </div>
+
+                    <div class="column p-6">
+                        <figure class="image container">
+                            <img src="/assets/logo/usatoday.svg" alt="USA Today logo">
+                        </figure>
+                    </div>
+                </div>
+            </div>
+
+            <div class="column">
+                <div class="columns is-mobile is-variable is-1-mobile">
+                    <div class="column p-6">
+                        <figure class="image container">
+                            <img src="/assets/logo/reuters.svg" alt="Reuters logo">
+                        </figure>
+                    </div>
+
+                    <div class="column p-6">
+                        <figure class="image container">
+                            <img src="/assets/logo/bloomberg.svg" alt="Bloomberg logo">
+                        </figure>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div id="row2" class="columns">
+            <div class="column">
+                <div class="columns is-mobile is-variable is-1-mobile">
+                    <div class="column p-6">
+                        <figure class="image container">
+                            <img src="/assets/logo/starz.svg" alt="STARZ logo">
+                        </figure>
+                    </div>
+
+                    <div class="column p-6">
+                        <figure class="image container">
+                            <img src="/assets/logo/pbs.svg" alt="Public Broadcasting Service logo">
+                        </figure>
+                    </div>
+                </div>
+            </div>
+
+            <div class="column">
+                <div class="columns is-mobile is-variable is-1-mobile">
+                    <div class="column p-6">
+                        <figure class="image container">
+                            <img src="/assets/logo/spc.svg" alt="Student Price Card logo">
+                        </figure>
+                    </div>
+
+                    <div class="column p-6">
+                        <figure class="image container">
+                            <img src="/assets/logo/redbull.svg" alt="Redbull logo">
+                        </figure>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div id="row3" class="columns">
+            <div class="column">
+                <div class="columns is-mobile is-variable is-1-mobile">
+                    <div class="column p-6">
+                        <figure class="image container">
+                            <img src="/assets/logo/ogilvy.svg" alt="Ogilvy logo">
+                        </figure>
+                    </div>
+
+                    <div class="column p-6">
+                        <figure class="image container">
+                            <img src="/assets/logo/plastic.svg" alt="Plastic Mobile logo">
+                        </figure>
+                    </div>
+                </div>
+            </div>
+
+            <div class="column">
+                <div class="columns is-mobile is-variable is-1-mobile">
+                    <div class="column p-6">
+                        <figure class="image container">
+                            <img src="/assets/logo/bnotions.svg" alt="Symbility logo">
+                        </figure>
+                    </div>
+
+                    <div class="column p-6">
+                        <figure class="image container">
+                            <img src="/assets/logo/twitch.svg" alt="Twitch.tv logo">
+                        </figure>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div id="row4" class="columns">
+            <div class="column">
+                <div class="columns is-mobile is-variable is-1-mobile">
+                    <div class="column p-6">
+                        <figure class="image container">
+                            <img src="/assets/logo/dell.svg" alt="Dell logo">
+                        </figure>
+                    </div>
+
+                    <div class="column p-6">
+                        <figure class="image container">
+                            <img src="/assets/logo/microsoft.svg" alt="Microsoft logo">
+                        </figure>
+                    </div>
+                </div>
+            </div>
+
+            <div class="column">
+                <div class="columns is-mobile is-variable is-1-mobile">
+                    <div class="column p-6">
+                        <figure class="image container">
+                            <img src="/assets/logo/cisco.svg" alt="Cisco logo">
+                        </figure>
+                    </div>
+
+                    <div class="column p-6">
+                        <figure class="image container">
+                            <img src="/assets/logo/latam.svg" alt="LATAM Airlines logo">
+                        </figure>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ components/Contact.vue
@@ -0,0 +1,24 @@
+<template>
+    <section id="cta" class=" mb-0">
+        <div class="container py-6">
+            <div class="has-text-centered pb-4">
+                <h1 class="title has-text-white">Say Hello</h1>
+            </div>
+            <div class="level is-centered">
+                <div class="level-item mb-6">
+                    <NuxtLink to="mailto:contact@philchoi.ca">
+                        <button class="button is-link is-medium is-outlined">
+                            <p><strong>Send an email</strong></p>
+                        </button>
+                    </NuxtLink>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped>
+</style>

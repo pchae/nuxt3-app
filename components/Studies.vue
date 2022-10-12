16eebf6cf58a53bf7a64749a6848ad380b91debb
pchae <20016424+pchae@users.noreply.github.com>
2022-10-12 16:05:43 -0400

    refactor img tags to include draggable, width, height, and loading params

--- components/Studies.vue
+++ components/Studies.vue
@@ -11,7 +11,12 @@
                         <!-- // Replace router link -->
                         <NuxtLink to="/project">
                             <figure class="image container">
-                                <img src="/assets/thumbs/engage.png" alt="Engage: Content Studio">
+                                <picture>
+                                    <source type="image/webp" srcset="/assets/thumbs/engage.webp">
+                                    <source type="image/png" srcset="/assets/thumbs/engage.png">
+                                    <img src="/assets/thumbs/engage.webp" alt="Engage: Content Studio SaaS screenshot."
+                                        draggable="false" width="800" height="600" loading="lazy">
+                                </picture>
                             </figure>
                             <div class="has-text-white">
                                 <p class="is-size-6">Engage: Content Studio</p>
@@ -23,7 +28,12 @@
                         <!-- // Replace router link -->
                         <NuxtLink to="/tos">
                             <figure class="image container">
-                                <img src="/assets/thumbs/spc.png" alt="Student Price Card">
+                                <picture>
+                                    <source type="image/webp" srcset="/assets/thumbs/spc.webp">
+                                    <source type="image/png" srcset="/assets/thumbs/spc.png">
+                                    <img src="/assets/thumbs/spc.webp" alt="Student Price Card mobile app screenshot."
+                                        draggable="false" width="800" height="600" loading="lazy">
+                                </picture>
                             </figure>
                             <div class="has-text-white">
                                 <p class="is-size-6">Student Price Card</p>
@@ -37,9 +47,14 @@
                 <div class="columns is-mobile is-variable is-1-mobile">
                     <div class="column p-1">
                         <!-- // Replace router link -->
-                        <NuxtLink to="/project">
+                        <NuxtLink to="/engage">
                             <figure class="image container">
-                                <img src="/assets/thumbs/pbs.png" alt="Public Broadcasting Service">
+                                <picture>
+                                    <source type="image/webp" srcset="/assets/thumbs/pbs.webp">
+                                    <source type="image/png" srcset="/assets/thumbs/pbs.png">
+                                    <img src="/assets/thumbs/pbs.webp" alt="Public Broadcasting Service Surface app screenshot."
+                                        draggable="false" width="800" height="600" loading="lazy">
+                                </picture>
                             </figure>
                             <div class="has-text-white">
                                 <p class="is-size-6">PBS</p>
@@ -51,7 +66,8 @@
                         <!-- // Replace router link -->
                         <NuxtLink to="/tos">
                             <figure class="image container">
-                                <img src="/assets/thumbs/ds.jpg" alt="Design Systems">
+                                <img src="/assets/thumbs/ds.jpg" alt="Design Systems" draggable="false" width="800"
+                                    height="600" loading="lazy">
                             </figure>
                             <div class="has-text-white">
                                 <p class="is-size-6">Design Systems</p>

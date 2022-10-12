3932493cc1bf3e234ff41029a4c1535b616512a5
pchae <20016424+pchae@users.noreply.github.com>
2022-10-12 18:54:20 -0400

    change store id formatting

--- store/client.js
+++ store/client.js
@@ -2,15 +2,15 @@ import { defineStore } from 'pinia'
 
 import clientJson from '@/json/clients.json'
 
-export const useClientStore = defineStore('clientLogos', {
-
+export const useClientStore = defineStore({
+    id: 'clientLogos',
     state: () => {
         return {
             clientJson
         }
     },
     getters: {
-//        clients: state => state.clientList,
+//        clients: state => state.clientList
     },
     actions: {}
 })

45a27d7b45a782884e7dcf3964d4c5d02963b163
pchae <20016424+pchae@users.noreply.github.com>
2022-10-23 15:23:37 -0400

    replace empty text class
    increased paragraph text size

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,10 +1,10 @@
 <template>
-  <section class="hero has-background-black">
-    <div class="hero-body">
+  <section class="container is-widescreen py-6 px-4">
+    <div class="content is-medium">
       <h1 class="title has-text-white">
         Phil Choi is a Product Designer based in Toronto.
       </h1>
-      <h2 class="subtitle has-text-grey-light">
+      <h2 class="is-size-5 has-text-weight-normal has-text-grey-light">
         Principal Product Designer at <NuxtLink to="https://flipboard.com/" target="_blank" rel="noopener">Flipboard</NuxtLink>. Previously at <NuxtLink to="https://momentumdesignlab.com" target="_blank" rel="noopener">Momentum</NuxtLink> & RBC Ventures.
       </h2>
     </div>

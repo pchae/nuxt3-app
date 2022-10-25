497b9754ec07d91573aecde3c0b024051b15b661
pchae <20016424+pchae@users.noreply.github.com>
2022-10-25 01:32:31 -0400

    create temp assets

---
+++ components/ProjectList.vue
@@ -0,0 +1,31 @@
+<template>
+    <section id="work" class="container py-6 px-4">
+        <div class="content has-text-centered mt-6">
+            <h1 class="title has-text-white">Case Studies</h1>
+        </div>
+
+        <div id="grid" class="is-flex is-flex-wrap-wrap">
+            <div class="media" v-for="item in projectStore.projectJson.projects" :key="item.id">
+                <figure class="image">
+                    <img :src="item.imgSrc" :alt="item.imgAlt" :width="item.width" :height="item.height">
+                </figure>
+            </div>
+        </div>
+
+    </section>
+</template>
+
+<script lang="ts">
+    import { useProjectStore } from "~/store/project";
+
+    export default {
+        setup() {
+            const projectStore = useProjectStore()
+
+            return { projectStore }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+</style>

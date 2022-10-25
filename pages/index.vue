218cd1499f1b299a5a808ffa8f6f9d2a72a66bc0
pchae <20016424+pchae@users.noreply.github.com>
2022-10-25 03:31:48 -0400

    change section colours and names

--- pages/index.vue
+++ pages/index.vue
@@ -10,11 +10,11 @@
 
         <Hero />
 
-        <section id="engage" class="section is-medium has-background-black-ter">
+        <section id="engage" class="section is-medium has-background-black">
             <div class="hero-body">
                 <div class="container">
                 <h1 class="title has-text-white">
-                    Engage Content Studio
+                    Engage: Content Studio
                 </h1>
                 <NuxtLink to="/engage">
                     <button class="button is-link is-medium">
@@ -25,13 +25,28 @@
             </div>
         </section>
 
-        <section id="c51" class="section is-medium has-background-grey-darker">
+        <section id="starz" class="section is-medium has-background-black-bis">
             <div class="hero-body">
                 <div class="container">
                 <h1 class="title has-text-white">
                     Starz Play
                 </h1>
                 <NuxtLink to="/starz">
+                    <button class="button is-link is-medium">
+                        <p><strong>View project</strong></p>
+                    </button>
+                </NuxtLink>
+            </div>
+            </div>
+        </section>
+
+        <section id="starz" class="section is-medium has-background-black">
+            <div class="hero-body">
+                <div class="container">
+                <h1 class="title has-text-white">
+                    Project Template
+                </h1>
+                <NuxtLink to="/project">
                     <button class="button is-link is-medium">
                         <p><strong>View project</strong></p>
                     </button>

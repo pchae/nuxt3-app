b47bc401777c98fda667008bda491986019e5b72
pchae <20016424+pchae@users.noreply.github.com>
2022-10-25 02:54:16 -0400

    update text content and images

--- pages/starz.vue
+++ pages/starz.vue
@@ -2,7 +2,7 @@
     <div class="">
 
         <Head>
-            <Title>Phil Choi - Starz Play</Title>
+            <Title>Phil Choi - Starz Play & Encore</Title>
             <Meta name="description" content="About Phil Choi, Product Designer based in Toronto" />
         </Head>
         <Nav />
@@ -19,22 +19,17 @@
                 <h1 class="title has-text-white">Overview</h1>
             </div>
             <div class="content">
-                <p class="is-size-5 has-text-weight-normal has-text-grey-light">Engage is a multi-channel social media
-                    engagement tool used by news media, sports teams and brands to promote live dynamic content and
-                    drive audience participation.</p>
-            </div>
-            <div class="content">
-                <p class="is-size-5 has-text-weight-normal has-text-grey-light">Upon joining as Design Lead, I made it a
-                    priority to establish a close relationship with Customer Support Managers and their clients. By
-                    taking the time to address outstanding issues and expected timelines, we were able to create a
-                    buffer that allowed us to meet the business demands of simultaneously overhauling the user
-                    experiences of multiple SaaS products while continuing to add value to their legacy versions.</p>
+                <p class="is-size-5 has-text-weight-normal has-text-grey-light">Starz is an American premium cable TV
+                    network competing with Netflix, HBO and Showtime. In an attempt to keep up with their rivals, Starz
+                    had previously launched streaming apps but struggled with activations and growth. It was clear they
+                    needed a fresh perspective and business model, one that would resonate with consumers by catering to
+                    diverse lifestyles and needs.</p>
             </div>
             <div class="content">
                 <picture>
-                    <source type="image/webp" srcset="/assets/project/engage/engage.webp">
-                    <source type="image/png" srcset="/assets/project/engage/engage.png">
-                    <img src="/assets/project/engage/engage.webp" alt="Native app design examples." draggable="false"
+                    <source type="image/webp" srcset="/assets/project/starz/movie.webp">
+                    <source type="image/png" srcset="/assets/project/starz/movie.jpg">
+                    <img src="/assets/project/starz/movie.webp" alt="Starz home screen movies tab" draggable="false"
                         width="1920" height="1080">
                 </picture>
             </div>
@@ -46,154 +41,72 @@
             </div>
         </section>
 
-        <section id="challenges" class="container py-6 px-4">
+        <section id="objectives" class="container py-6 px-4">
             <div class="content">
-                <h1 class="title has-text-white">Challenges</h1>
+                <h1 class="title has-text-white">Objectives</h1>
             </div>
             <div class="content">
-                <p class="is-size-5 has-text-weight-normal has-text-grey-light">Over the years, the web app went through
-                    several redesigns that had not been fully implemented and the source code to the native mobile apps
-                    had been lost. The Engineering team recommended building a Progressive Web App (PWA) to replace the
-                    mobile apps, leverage the team's expertise, and have a shared codebase for both mobile platforms.
-                    This would also allow us to refactor the web application at the feature level without negatively
-                    impacting the experience for clients.</p>
+                <p class="is-size-5 has-text-weight-normal has-text-grey-light">The goals of the team were to design,
+                    build, and deploy 3 versions of the app in near simultaneous launch windows. The end to end design
+                    was alloted 3 months to produce Roku, Amazon FireTV and Samsung SmartTVs experiences which were all
+                    new platforms for the Digiflare team.</p>
+                <p class="is-size-5 has-text-weight-normal has-text-grey-light">The components allowed us to work at a
+                    high fidelity and sped up the production of consistent, accurate layouts. Since measurements were
+                    baked in, it also facilitated a quick turn-around time for creating production-ready UI elements.
+                </p>
             </div>
             <div class="">
                 <picture>
-                    <source type="image/webp" srcset="/assets/project/engage/m_redesign.webp">
-                    <source type="image/png" srcset="/assets/project/engage/m_redesign.png">
-                    <img src="/assets/project/engage/m_redesign.webp" alt="Native app design examples."
-                        draggable="false" width="1600" height="1013" loading="lazy">
+                    <source type="image/webp" srcset="/assets/project/starz/wire-welcome.webp">
+                    <source type="image/png" srcset="/assets/project/starz/wire-welcome.png">
+                    <img src="/assets/project/starz/wire-welcome.webp" alt="RENAME."
+                        draggable="false" width="1920" height="1070" loading="lazy">
                 </picture>
             </div>
         </section>
 
-        <section id="analytics" class="container py-6 px-4">
+        <section id="visual-design" class="container py-6 px-4">
             <div class="content">
-                <h1 class="title has-text-white">Analytics & Measurement</h1>
+                <h1 class="title has-text-white">Visual Design</h1>
             </div>
             <div class="content">
-                <p class="is-size-5 has-text-weight-normal has-text-grey-light">I worked with Product owners to
-                    introduce and configure Pendo.ai as our primary measurement tool across the organization. This
-                    allowed us to accurately validate high traffic features, identify pain points, and isolate unused
-                    features to sunset. Meanwhile, initiatives like client outreach with CSMs allowed us to uncover new
-                    issues which led to a reprioritization of the roadmap.</p>
+                <p class="is-size-5 has-text-weight-normal has-text-grey-light">We created a dark, minimalist user
+                    interface with high contrast ratios to place emphasis on the poster art and reduce eye strain. It
+                    was also essential that we tried to capture the essence of going to a movie theatre while
+                    incorporating Starz branding.</p>
             </div>
             <div class="">
                 <picture>
-                    <source type="image/webp" srcset="/assets/project/engage/pendo.webp">
-                    <source type="image/png" srcset="/assets/project/engage/pendo.jpg">
-                    <img src="/assets/project/engage/pendo.webp" alt="Pendo.ai analytics tool screenshot."
-                        draggable="false" width="1600" height="960" loading="lazy">
-                </picture>
-            </div>
-        </section>
-
-        <section id="wireframes" class="container py-6 px-4">
-            <div class="content">
-                <h1 class="title has-text-white">Mobile-first wireframes</h1>
-            </div>
-            <div class="content">
-                <p class="is-size-5 has-text-weight-normal has-text-grey-light">The goal of the PWA was to reimagine
-                    Engage's Content Studio feature set in a new Mobile-first experience. Not only would this allow
-                    users to curate and moderate live Social Media events, but clients in the Journalism space would
-                    finally be able to create, edit, and post written and rich-media content live and in real-time from
-                    the source.</p>
-            </div>
-            <div class="">
-                <picture>
-                    <source type="image/webp" srcset="/assets/project/engage/wires.webp">
-                    <source type="image/png" srcset="/assets/project/engage/wires.png">
-                    <img src="/assets/project/engage/wires.webp" alt="Figma mobile-first wireframes." draggable="false"
-                        width="1600" height="800" loading="lazy">
-                </picture>
-            </div>
-        </section>
-
-        <section id="pwa" class="container py-6 px-4">
-            <div class="content">
-                <h1 class="title has-text-white">Progressive Web App</h1>
-            </div>
-            <div class="content">
-                <p class="is-size-5 has-text-weight-normal has-text-grey-light">The experience was built with
-                    React-Native and StoryBook to create a 1:1 mirror of my Design System. These components were
-                    engineered from the ground-up to be the foundational building blocks that would revamp the entire
-                    Engage product as well as Scribblelive's SaaS suite ambitions.</p>
-            </div>
-            <div class="">
-                <picture>
-                    <source type="image/webp" srcset="/assets/project/engage/m_cs1.webp">
-                    <source type="image/png" srcset="/assets/project/engage/m_cs1.png">
-                    <img src="/assets/project/engage/m_cs1.webp" alt="Figma mobile-first wireframes." draggable="false"
-                        width="2000" height="1040" loading="lazy">
-                </picture>
-            </div>
-        </section>
-
-        <section id="prototype" class="container py-6 px-4">
-            <div class="content">
-                <h1 class="title has-text-white">Prototyping & Usability Tests</h1>
-            </div>
-            <div class="content">
-                <p class="is-size-5 has-text-weight-normal has-text-grey-light">To validate each user story, I created a
-                    series of user flows and interactive prototypes to document and test conditional logic, complex
-                    micro-interactions, and dynamic workflows. Validation was done in-house with clients and via remote
-                    tools like usertesting.com.</p>
-            </div>
-            <div class="">
-                <picture>
-                    <source type="image/webp" srcset="/assets/project/engage/prototype.webp">
-                    <source type="image/png" srcset="/assets/project/engage/prototype.jpg">
-                    <img src="/assets/project/engage/prototype.webp"
-                        alt="Figma prototype usability testing session photo." draggable="false" width="1600"
-                        height="800" loading="lazy">
-                </picture>
-            </div>
-        </section>
-
-        <section id="clients" class="container py-6 px-4">
-            <div class="content">
-                <h1 class="title has-text-white">Client Feature Requests</h1>
-            </div>
-            <div class="content">
-                <p class="is-size-5 has-text-weight-normal has-text-grey-light">The sales-focused nature of the company
-                    created a continuous stream of unique feature requests from clients like DELL EMC and Redbull. One
-                    of Dell's requests (shown below) was to create a new modular and embeddable experience linking live
-                    streams with Engage's social media integrations.</p>
-            </div>
-            <div class="">
-                <picture>
-                    <source type="image/webp" srcset="/assets/project/engage/engage-new_ui.webp">
-                    <source type="image/png" srcset="/assets/project/engage/engage-new_ui.jpg">
-                    <img src="/assets/project/engage/engage-new_ui.webp" alt="Figma mobile-first wireframes."
+                    <source type="image/webp" srcset="/assets/project/starz/ui-feature-firetv.webp">
+                    <source type="image/png" srcset="/assets/project/starz/ui-feature-firetv.jpg">
+                    <img src="/assets/project/starz/ui-feature-firetv.webp" alt="RENAME."
                         draggable="false" width="1920" height="1080" loading="lazy">
                 </picture>
             </div>
         </section>
 
-        <section id="social" class="container py-6 px-4">
+        <section id="design-handoff" class="container py-6 px-4">
             <div class="content">
-                <h1 class="title has-text-white">Social Media Components</h1>
+                <h1 class="title has-text-white">Design Handoff & Extras</h1>
             </div>
             <div class="content">
-                <p class="is-size-5 has-text-weight-normal has-text-grey-light">One of Engage's core features is the
-                    ability to add, curate, and moderate multiple social media sources into a single feed known as a
-                    'vertical'. The app achieved this by recreating the layouts and populating the content with cached
-                    copies of the original social media posts from platforms like Twitter, Instagram, Youtube, and
-                    Facebook. All of the modules needed to be upyated and redesigned to add responsiveness, pass WCAG
-                    2.1 accessibility guidelines, and match the latest UIs of the social media platforms. Custom modules
-                    for video and editorial content were also designed and developed to fulfill requests from the
-                    company's media clients.</p>
+                <p class="is-size-5 has-text-weight-normal has-text-grey-light">Automating design to development
+                    hand-off was a new concept at the time, but we took a calculated risk to get ahead of the deadlines
+                    by adding Zeplin.io to our design stack. This saved multiple sprints worth of time by automating
+                    measurement specifications and removed what would have been a costly bottleneck for development. The
+                    remaining time was used to create reusable components, add quality of life enhancements and
+                    personalization elements.</p>
             </div>
             <div class="">
                 <picture>
-                    <source type="image/webp" srcset="/assets/project/engage/spec.webp">
-                    <source type="image/png" srcset="/assets/project/engage/spec.png">
-                    <img src="/assets/project/engage/spec.webp" alt="Spec examples for social media components."
-                        draggable="false" width="1600" height="840" loading="lazy">
+                    <source type="image/webp" srcset="/assets/project/starz/resume.webp">
+                    <source type="image/png" srcset="/assets/project/starz/resume.jpg">
+                    <img src="/assets/project/starz/resume.webp" alt="RENAME." draggable="false"
+                        width="1920" height="1080" loading="lazy">
                 </picture>
             </div>
         </section>
+
 
         <ProjectList />
 

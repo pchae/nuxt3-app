41d82e61e2c8ff704701c370389d068115ed0fe2
pchae <20016424+pchae@users.noreply.github.com>
2022-10-30 19:36:01 -0400

    add page transition styles

--- app.vue
+++ app.vue
@@ -3,13 +3,22 @@
 </template>
 
 <script setup>
-  const nuxtApp = useNuxtApp()
+const nuxtApp = useNuxtApp()
 
-  nuxtApp.hook("page:finish", () => {
-    window.scrollTo(0, 0)
-  })
+nuxtApp.hook("page:finish", () => {
+  window.scrollTo(0, 0)
+})
 </script>
 
 <style>
+  .page-enter-active,
+  .page-leave-active {
+    transition: all 0.2s;
+  }
 
+  .page-enter-from,
+  .page-leave-to {
+    opacity: 0;
+    transition: all 0.2s;
+  }
 </style>

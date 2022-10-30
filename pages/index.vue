776517db1cbc23a5cb6860796b2221349019f20e
pchae <20016424+pchae@users.noreply.github.com>
2022-10-30 19:10:13 -0400

    refactor project links into 2 column grid

--- pages/index.vue
+++ pages/index.vue
@@ -10,48 +10,143 @@
 
         <Hero />
 
-        <section id="engage" class="section is-medium has-background-black">
-            <div class="hero-body">
-                <div class="container">
-                <h1 class="title has-text-white">
-                    Engage: Content Studio
-                </h1>
-                <NuxtLink to="/engage">
-                    <button class="button is-link is-medium">
-                        <p><strong>View project</strong></p>
-                    </button>
-                </NuxtLink>
-            </div>
-            </div>
-        </section>
+        <section id="grid" class="container has-background-black">
+            <div class="columns">
+                <div class="column">
+                    <div class="box has-background-black-ter">
+                        <div class="media">
+                            <picture>
+                                <source type="image/webp" srcset="~/assets/thumbs/engage.webp">
+                                <source type="image/png" srcset="~/assets/thumbs/engage.jpg">
+                                <img src="~/assets/thumbs/engage.webp" alt="RENAME." draggable="false" width="1920"
+                                    height="1080" loading="lazy">
+                            </picture>
+                        </div>
+                        <div class="pb-2">
+                            <h1 class="title has-text-white">
+                                Content Studio
+                            </h1>
+                        </div>
 
-        <section id="starz" class="section is-medium has-background-black-bis">
-            <div class="hero-body">
-                <div class="container">
-                <h1 class="title has-text-white">
-                    Starz Play
-                </h1>
-                <NuxtLink to="/starz">
-                    <button class="button is-link is-medium">
-                        <p><strong>View project</strong></p>
-                    </button>
-                </NuxtLink>
-            </div>
-            </div>
-        </section>
+                        <div class="tags">
+                            <span class="tag is-success">
+                                Enterprise SaaS
+                            </span>
+                            <span class="tag is-success">
+                                PWA
+                            </span>
+                            <span class="tag is-success">
+                                React Native
+                            </span>
+                        </div>
 
-        <section id="starz" class="section is-medium has-background-black">
-            <div class="hero-body">
-                <div class="container">
-                <h1 class="title has-text-white">
-                    Project Template
-                </h1>
-                <NuxtLink to="/project">
-                    <button class="button is-link is-medium">
-                        <p><strong>View project</strong></p>
-                    </button>
-                </NuxtLink>
-            </div>
+                        <NuxtLink to="/engage">
+                            <button class="button is-link is-medium">
+                                <p><strong>View project</strong></p>
+                            </button>
+                        </NuxtLink>
+                    </div>
+
+                    <div class="box has-background-black-ter">
+                        <div class="media">
+                            <picture>
+                                <source type="image/webp" srcset="~/assets/thumbs/pbs.webp">
+                                <source type="image/png" srcset="~/assets/thumbs/pbs.jpg">
+                                <img src="~/assets/thumbs/pbs.webp" alt="RENAME." draggable="false" width="1920"
+                                    height="1080" loading="lazy">
+                            </picture>
+                        </div>
+                        <div class="pb-2">
+                            <h1 class="title has-text-white">
+                                PBS
+                            </h1>
+                        </div>
+                        <div class="tags">
+                            <span class="tag is-success">
+                                Microsoft Lumia
+                            </span>
+                            <span class="tag is-success">
+                                Microsoft Surface
+                            </span>
+                        </div>
+
+                        <NuxtLink to="/engage">
+                            <button class="button is-link is-medium">
+                                <p><strong>View project</strong></p>
+                            </button>
+                        </NuxtLink>
+                    </div>
+                </div>
+
+                <div class="column">
+                    <div class="box has-background-black-ter">
+                        <div class="media">
+                            <picture>
+                                <source type="image/webp" srcset="~/assets/thumbs/starz.webp">
+                                <source type="image/png" srcset="~/assets/thumbs/starz.jpg">
+                                <img src="~/assets/thumbs/starz.webp" alt="RENAME." draggable="false" width="1920"
+                                    height="1080" loading="lazy">
+                            </picture>
+                        </div>
+                        <div class="pb-2">
+                            <h1 class="title has-text-white">
+                                Starz Play
+                            </h1>
+                        </div>
+
+                        <div class="tags">
+                            <span class="tag is-success">
+                                Roku
+                            </span>
+                            <span class="tag is-success">
+                                Samsung SmartTV
+                            </span>
+                            <span class="tag is-success">
+                                Amazon FireTV
+                            </span>
+                        </div>
+
+                        <NuxtLink to="/starz">
+                            <button class="button is-link is-medium">
+                                <p><strong>View project</strong></p>
+                            </button>
+                        </NuxtLink>
+                    </div>
+
+                    <div class="box has-background-black-ter">
+                        <div class="media">
+                            <picture>
+                                <source type="image/webp" srcset="~/assets/thumbs/ds.webp">
+                                <source type="image/png" srcset="~/assets/thumbs/ds.jpg">
+                                <img src="~/assets/thumbs/ds.webp" alt="RENAME." draggable="false" width="1920"
+                                    height="1080" loading="lazy">
+                            </picture>
+                        </div>
+                        <div class="pb-2">
+                            <h1 class="title has-text-white">
+                                Design Systems
+                            </h1>
+                        </div>
+
+                        <div class="tags">
+                            <span class="tag is-success">
+                                Figma
+                            </span>
+                            <span class="tag is-success">
+                                Sketch
+                            </span>
+                            <span class="tag is-success">
+                                Storybook.js
+                            </span>
+                        </div>
+
+                        <NuxtLink to="/project">
+                            <button class="button is-outlined is-link is-medium">
+                                <p><strong>Request Access</strong></p>
+                            </button>
+                        </NuxtLink>
+                    </div>
+                </div>
             </div>
         </section>
 
@@ -65,6 +160,10 @@
 </script>
 
 
-<style>
+<style lang="scss" scoped>
 
+.tag:not(body).is-success {
+    background-color: #1CD0A0;
+    color: #000;
+}
 </style>

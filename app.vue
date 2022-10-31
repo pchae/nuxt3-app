3f153baf8e9eaf5ea21c6c8a6b5bff46a0c031ce
pchae <20016424+pchae@users.noreply.github.com>
2022-10-30 21:01:36 -0400

    remove scrollTo script

--- app.vue
+++ app.vue
@@ -3,22 +3,18 @@
 </template>
 
 <script setup>
-const nuxtApp = useNuxtApp()
 
-nuxtApp.hook("page:finish", () => {
-  window.scrollTo(0, 0)
-})
 </script>
 
 <style>
   .page-enter-active,
   .page-leave-active {
-    transition: all 0.2s;
+    transition: all 0.3s;
   }
 
   .page-enter-from,
   .page-leave-to {
     opacity: 0;
-    transition: all 0.2s;
+    transition: all 0.3s;
   }
 </style>

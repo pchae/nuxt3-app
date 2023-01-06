657889f8a03b94f7b73a773173b64bb6d59b48ae
pchae <20016424+pchae@users.noreply.github.com>
2023-01-06 01:16:07 -0500

    update job title

--- components/Hero.vue
+++ components/Hero.vue
@@ -5,7 +5,7 @@
         Phil Choi is a Product Designer based in Toronto.
       </h1>
       <h2 class="is-size-5 has-text-weight-normal has-text-grey-light">
-        Principal Product Designer at <NuxtLink to="https://flipboard.com/" target="_blank" rel="noopener">Flipboard</NuxtLink>. Previously at <NuxtLink to="https://momentumdesignlab.com" target="_blank" rel="noopener">Momentum</NuxtLink> & <NuxtLink to="https://www.rbcx.com/" target="_blank" rel="noopener">RBC Ventures</NuxtLink>.
+        Staff Product Designer at <NuxtLink to="https://flipboard.com/" target="_blank" rel="noopener">Flipboard</NuxtLink>.
       </h2>
     </div>
   </section>
